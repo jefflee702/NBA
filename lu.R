c42e8dacea76d486b332ec3e8f8f1e3a31ca99a9
jefflee702 <jefflee7020@gmail.com>
2023-03-13 21:03:03 -0700

    lu in r

--- lu.R
+++ lu.R
@@ -2,9 +2,9 @@ library(Matrix)
 
 #Read in data
 data2 <- read.csv(file = 'data/all_seasons.csv')
-players_data <- data2[,c("net_rating", "age", "player_height", "player_weight", "pts", "reb", "ast","oreb_pct","dreb_pct","usg_pct","ts_pct","ast_pct")]
-#players_data <- data2[data2$draft_round != "Undrafted", ]
-#players_data$draft_round <- as.numeric(players_data$draft_round)
+players_data <- data2[data2$draft_round != "Undrafted", ]
+players_data$draft_round <- as.numeric(players_data$draft_round)
+players_data <- players_data[,c("net_rating", "age", "player_height", "player_weight", "pts", "reb", "ast","oreb_pct","dreb_pct","usg_pct","ts_pct","ast_pct")]
 
 X <- cbind(1, as.matrix(players_data[, 2:12]))
 y <- players_data[, 1]
